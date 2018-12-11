f5d3e86ea17d72cb0f99e35806a4a753ac1a2045
unknown <kflint0068@gmail.com>
2018-12-11 15:17:58 -0800

    added title code to getJSON (doesn't work yet)

--- blog_script.js
+++ blog_script.js
@@ -2,6 +2,13 @@ $.getJSON("posts/posts.json", function (data) {
     $.each(data, function (index, item) {
         let article = $("<div></div>");
         article.addClass("article");
+
+        let title = $("<h2></h2>");
+        title.text(item.title);
+        console.log(title);
+
+        // load refreshes it? but doesn't work if it goes after article.load either.
+        article.append(title);
         article.load(item.source);
         $(".content-div").append(article);
     });
