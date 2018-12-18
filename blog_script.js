c117f4ee93eec5904822ecf43d3beec874310f2a
unknown <kflint0068@gmail.com>
2018-12-17 17:10:47 -0800

    added date to blog posts

--- blog_script.js
+++ blog_script.js
@@ -4,10 +4,13 @@ $.getJSON("posts/posts.json", function (data) {
         article.addClass("article");
 
         article.attr("data-title", item.title);
+        article.attr("data-date", item.date);
         article.load(item.source, function () {
             let title = $("<h2></h2>");
+            let date = $("<p></p>");
             title.text(this.getAttribute("data-title"));
-            console.log(title);
+            date.text(this.getAttribute("data-date"));
+            this.prepend(date.get(0));
             this.prepend(title.get(0));
         });
         $(".content-div").append(article);
